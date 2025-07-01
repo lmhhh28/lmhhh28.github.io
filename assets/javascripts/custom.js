a0404f20f6adbc451da2c6102ed4a02a89709738
lmhhh28 <14861076+lmhhh28@user.noreply.gitee.com>
2025-07-01 11:51:30 +0800

    Deployed 0930440 with MkDocs version: 1.6.1

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -8,3 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+// 异步加载 KaTeX 后初始化
+window.addEventListener('load', function() {
+  renderMathInElement(document.body, {
+    delimiters: [
+      { left: '$$', right: '$$', display: true },
+      { left: '$', right: '$', display: true }
+    ],
+    // 限定渲染范围（提升性能）
+    ignoredElements: ['nav', 'header', 'footer'] 
+  });
+});
